ee52df705cd713e00a8674cf3006003a4e549a64
aerjay <aerjay.italia@gmail.com>
2020-12-28 10:13:34 -0700

    Convert spaces to tabs.

--- src/Game.js
+++ src/Game.js
@@ -3,15 +3,15 @@ import './Game.css'
 import Board from './Board'
 
 class Game extends React.Component {
-  render () {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-      </div>
-    )
-  }
+	render () {
+		return (
+			<div className="game">
+				<div className="game-board">
+					<Board />
+				</div>
+			</div>
+		)
+	}
 }
 
 export default Game
